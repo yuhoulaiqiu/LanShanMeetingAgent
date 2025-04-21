f0352048e16a73aac10f803290365ec6ce4ae6e2
yuhoulaiqiu <3240858086@qq.com>
2025-04-21 23:06:44 +0800

    feat:添加rag

--- service/summary/rag.go
+++ service/summary/rag.go
@@ -1,1 +1,38 @@
 package summary
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudwego/eino-ext/components/document/transformer/splitter/markdown"
+	"github.com/cloudwego/eino/schema"
+	"log"
+	"meetingagent/service/utils"
+)
+
+func Split(meetingID string) {
+	ctx := context.Background()
+	// 初始化分割器
+	splitter, err := markdown.NewHeaderSplitter(ctx, &markdown.HeaderConfig{
+		Headers: map[string]string{
+			"#":   "h1",
+			"##":  "h2",
+			"###": "h3",
+		},
+		TrimHeaders: false,
+	})
+	if err != nil {
+		panic(err)
+	}
+	doc, _ := utils.ReadMeetingSummaryText(meetingID)
+	docs := []*schema.Document{
+		{
+			ID:      meetingID,
+			Content: doc,
+		},
+	}
+	res, err := splitter.Transform(ctx, docs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+}
---
+++ service/summary/rag_test.go
@@ -0,0 +1,7 @@
+package summary
+
+import "testing"
+
+func TestSplit(t *testing.T) {
+	Split("meeting_20250421215612")
+}
