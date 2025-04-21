e51723cb54a5a0191bdc21dc6271e87e8ee3e2b4
yuhoulaiqiu <3240858086@qq.com>
2025-04-22 01:57:31 +0800

    update:修改chat prompt

--- service/chat/prompt.go
+++ service/chat/prompt.go
@@ -8,6 +8,7 @@ import (
 func NewChatTemplate() *prompt.DefaultChatTemplate {
 	template := prompt.FromMessages(schema.FString,
 		schema.SystemMessage("你是一个智能助手，根据会议摘要，回答用户问题"),
+		schema.SystemMessage("你只能回答本次用户会议相关的问题，其余问题一律委婉地拒绝"),
 		schema.SystemMessage("现在的时间是: {current_time}"),
 		schema.SystemMessage("从向量数据库中获取的相关上下文: {context}"),
 		schema.MessagesPlaceholder("chat_history", true),
